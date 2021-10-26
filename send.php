7ca7d99f562ae29dc2e750e3734bf8dc8472cde6
Orloviss <44546222+Orloviss@users.noreply.github.com>
2021-10-27 01:18:29 +0300

    Update send.php

--- send.php
+++ send.php
@@ -1,9 +1,10 @@
 
+
 <?php
 // Файлы phpmailer
-require 'phpmailer/PHPMailer.php';
-require 'phpmailer/SMTP.php';
-require 'phpmailer/Exception.php';
+require './phpmailer/PHPMailer.php';
+require './phpmailer/SMTP.php';
+require './phpmailer/Exception.php';
 
 // Переменные, которые отправляет пользователь
 $name = $_POST['name'];
